d7c8b29cdd72b09a0be554ae7ed54de4cf104e96
ChenQiang <63850539+ChenQiang-AISS@users.noreply.github.com>
2023-01-05 22:27:24 +0900

    Update use_ngrok.sh

--- use_ngrok.sh
+++ use_ngrok.sh
@@ -1,5 +1,7 @@
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz 
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v\3-stable-linux-amd64.tgz 
 tar -xf $(ls *.tgz) 
+mkdir ~/.config/
+mkdir ~/.config/ngrok
 ln -s /etc/secrets/ngrok.yml ~/.config/ngrok/ngrok.yml
 # ./ngrok -log="$HOME/ngrok.log" tcp 22 
 ./ngrok tcp 22
