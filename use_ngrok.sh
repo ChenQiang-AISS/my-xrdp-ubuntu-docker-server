ddb7e7aac85bf85c4a7dcf3d08076dda5fd6b71f
ChenQiang <63850539+ChenQiang-AISS@users.noreply.github.com>
2023-01-05 22:44:20 +0900

    Update use_ngrok.sh

--- use_ngrok.sh
+++ use_ngrok.sh
@@ -1,7 +1,4 @@
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v\3-stable-linux-amd64.tgz 
 tar -xf $(ls *.tgz) 
-mkdir ~/.config/
-mkdir ~/.config/ngrok
-ln -s /etc/secrets/ngrok.yml ~/.config/ngrok/ngrok.yml
-# ./ngrok -log="$HOME/ngrok.log" tcp 22 
+./ngrok config add-authtoken $NGROK_KEY
 ./ngrok tcp 22
