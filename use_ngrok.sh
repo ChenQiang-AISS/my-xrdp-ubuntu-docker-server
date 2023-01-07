adb6fb5626b74dc54aa925ffd663296bfe462326
ChenQiang <63850539+ChenQiang-AISS@users.noreply.github.com>
2023-01-07 17:34:15 +0900

    Undo changes

--- use_ngrok.sh
+++ use_ngrok.sh
@@ -1,5 +1,4 @@
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v\3-stable-linux-amd64.tgz 
 tar -xf $(ls *.tgz) 
 ./ngrok config add-authtoken $NGROK_KEY
-python3 -m http.server 80 &
-./ngrok tcp 3389 &
+./ngrok tcp 3389 
