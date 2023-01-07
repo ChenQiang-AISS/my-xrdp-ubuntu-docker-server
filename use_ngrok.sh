3bfd7abe73b86d076a5e3220230c101d0c7ae67b
ChenQiang <63850539+ChenQiang-AISS@users.noreply.github.com>
2023-01-07 17:23:35 +0900

    tured to background process

--- use_ngrok.sh
+++ use_ngrok.sh
@@ -1,4 +1,5 @@
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v\3-stable-linux-amd64.tgz 
 tar -xf $(ls *.tgz) 
 ./ngrok config add-authtoken $NGROK_KEY
-./ngrok tcp 3389
+python3 -m http.server 80 &
+./ngrok tcp 3389 &
