16a29ed82b93f93e33fe90ff0d540a59aefb4edd
ChenQiang <63850539+ChenQiang-AISS@users.noreply.github.com>
2023-01-08 17:22:40 +0900

    Added python3-pip and wssh

--- useful_apps.sh
+++ useful_apps.sh
@@ -1,7 +1,8 @@
 apt update
-apt install htop curl wget p7zip-full p7zip-rar ttf-wqy-zenhei -y
+apt install htop curl wget p7zip-full p7zip-rar ttf-wqy-zenhei python3-pip -y
 
 dpkg --add-architecture i386
 apt update
 apt install wine64 -y
 
+pip install install webssh
